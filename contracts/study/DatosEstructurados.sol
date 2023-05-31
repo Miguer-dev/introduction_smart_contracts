bebfaf0676cd558253885184baee127a8ea39752
Miguer-dev <miguerdev@gmail.com>
2023-05-31 00:08:11 -0400

    Created contract "DatosEstructurados"

--- contracts/study/DatosEstructurados.sol
+++ contracts/study/DatosEstructurados.sol
@@ -4,15 +4,20 @@ pragma solidity >=0.7.0 <0.9.0;
 
 contract Clase {
     
+    enum Gender {FEMALE, MALE, NOBINARY}
+    
     struct Alumno {
         string nombre;
         uint documento;
+        Gender gender;
     }
     
     Alumno[] public alumnos;
+    mapping(string => uint) public notes;
     
     constructor() {
-        alumnos.push(Alumno({ nombre: "Miguel", documento: 66666 }));
+        alumnos.push(Alumno({ nombre: "Miguel", documento: 66666, gender: Gender.MALE}));
+        notes["Miguel"] = 100;
     }
     
 }
